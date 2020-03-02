a030de3b41d8d7c48114aed73585389c5baf8019
Leonardo Coelho <leonardo.coelho@inceres.com.br>
2020-03-02 10:39:38 -0300

    Finalizado teste do desafio o_primeiro_do_ranking

--- tests/test_o_primeiro_do_ranking.py
+++ tests/test_o_primeiro_do_ranking.py
@@ -1,5 +1,10 @@
+# encoding: utf-8
+
+import time
 import unittest
 from src.o_primeiro_do_ranking import o_primeiro_do_ranking
+
+from tests.entradas_e_saidas import es_o_primeiro_do_ranking
 
 
 class TestOPrimeiroDoRanking(unittest.TestCase):
@@ -9,5 +14,32 @@ class TestOPrimeiroDoRanking(unittest.TestCase):
         pontuacoes = [100, 100, 50, 40, 40, 20, 10]
         qtd_pontuacoes_do_joaozinho = 4
         pontuacoes_do_joaozinho = [5, 25, 50, 120]
+
         resposta = o_primeiro_do_ranking(qtd_pontuacoes, pontuacoes, qtd_pontuacoes_do_joaozinho, pontuacoes_do_joaozinho)
+
         self.assertEqual([6, 4, 2, 1], resposta)
+
+    def test_deve_retornar_posicoes_quando_classificacao_e_um_pouco_maior(self):
+        qtd_pontuacoes = es_o_primeiro_do_ranking.ENTRADA_TESTE_1['qtd_pontuacoes']
+        pontuacoes = list(map(int,es_o_primeiro_do_ranking.ENTRADA_TESTE_1['pontuacoes'].rstrip().split()))
+        qtd_pontuacoes_do_joaozinho = es_o_primeiro_do_ranking.ENTRADA_TESTE_1['qtd_pontuacoes_joaozinho']
+        pontuacoes_do_joaozinho = list(map(int, es_o_primeiro_do_ranking.ENTRADA_TESTE_1['pontuacoes_joaozinho'].rstrip().split()))
+
+        resposta = o_primeiro_do_ranking(qtd_pontuacoes, pontuacoes, qtd_pontuacoes_do_joaozinho, pontuacoes_do_joaozinho)
+
+        resposta_esperada = list(map(int, es_o_primeiro_do_ranking.SAIDA_TESTE_1.rstrip().split()))
+        self.assertEqual(resposta_esperada, resposta)
+
+    def test_deve_retornar_posicoes_rapidamnete_quando_classificacao_e_colossal(self):
+        qtd_pontuacoes = es_o_primeiro_do_ranking.ENTRADA_TESTE_3['qtd_pontuacoes']
+        pontuacoes = list(map(int,es_o_primeiro_do_ranking.ENTRADA_TESTE_3['pontuacoes'].rstrip().split()))
+        qtd_pontuacoes_do_joaozinho = es_o_primeiro_do_ranking.ENTRADA_TESTE_3['qtd_pontuacoes_joaozinho']
+        pontuacoes_do_joaozinho = list(map(int, es_o_primeiro_do_ranking.ENTRADA_TESTE_3['pontuacoes_joaozinho'].rstrip().split()))
+
+        tempo_de_inicio = time.time()
+        resposta = o_primeiro_do_ranking(qtd_pontuacoes, pontuacoes, qtd_pontuacoes_do_joaozinho, pontuacoes_do_joaozinho)
+        tempo_de_execucao = time.time() - tempo_de_inicio
+
+        resposta_esperada = list(map(int, es_o_primeiro_do_ranking.SAIDA_TESTE_3.rstrip().split()))
+        self.assertEqual(resposta_esperada, resposta)
+        self.assertTrue(tempo_de_execucao < 5) # Caso não consiga fazer em menos de 5 segundos, tente chegar o mais proximo disso
