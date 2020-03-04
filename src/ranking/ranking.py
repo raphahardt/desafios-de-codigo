c5a4edf82049bcba873b4a3af6ac1e13585bb888
Raphael Hardt Linux <raphael.hardt@gmail.com>
2020-03-04 14:04:43 -0300

    otimização funcionando bem, porém o código ainda não está amigavel e muito espaguete, vou ainda refatorar

---
+++ src/ranking/ranking.py
@@ -0,0 +1,15 @@
+
+class Ranking:
+    pontuacoes = []
+    qtd_pontuacoes = 0
+
+    rank = None
+    last_index = None
+
+    def __init__(self, pontuacoes, qtd_pontuacoes):
+        self.pontuacoes = pontuacoes
+        self.qtd_pontuacoes = qtd_pontuacoes
+
+    def iterar_em_pontuacoes(self):
+        # TODO: fazer a iteração aqui
+        pass
